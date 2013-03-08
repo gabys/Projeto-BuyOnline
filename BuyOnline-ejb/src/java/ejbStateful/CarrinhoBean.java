ac9c230cfb7a9f0345b43e3bcf514dfbca2e858d
GFactory <GFactory@192.168.1.101>
2013-03-08 16:04:23 -0300

    Reorganização de algumas classes

--- BuyOnline-ejb/src/java/ejbStateful/CarrinhoBean.java
+++ BuyOnline-ejb/src/java/ejbStateful/CarrinhoBean.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejbStateless;
+package ejbStateful;
 
 /**
  *
  * @author GFactory
  */
-public class CarrinhoBeanImpl {
+public class CarrinhoBean {
     
 }
